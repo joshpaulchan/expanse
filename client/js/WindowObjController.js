594049f5d914e0e887dbd3b258ab24e5d3403e7b
joshpaulchan <joshpaulchan@gmail.com>
2016-02-21 11:30:58 -0500

    Implement window creation and destruction logic
    I (almost fully) implmented the creation logi for the windows in the windowObj controller. I'm debating whether it should also insert objects into the scene, or whether that would be too many responsibilities for it.

---
+++ client/js/WindowObjController.js
@@ -0,0 +1,34 @@
+
+var WindowObjController = function(scene) {
+    
+    var windows;
+    var scene;
+    
+    this.init = function(scene) {
+        this.windows = [];
+    };
+    
+    this.createWindows = function(n, feeds) {
+        // TODO:
+    };
+    
+    this.createWindow = function(srcUrl) {
+        var wdo = new WindowObj();
+        this.windows.push(wdo);             // Track window
+        // this.scene.add(wdo.obj);            // Add window to scene
+        return wdo;
+    };
+    
+    this.destroyWindow = function(windowId) {
+        this.windows.filter(function(wo) {
+            if (wo.id == windowId) {
+                wo.close();
+            }
+            return (wo.id != windowId);
+        });
+    };
+    
+    this.init(scene);
+    
+    return this;
+};
