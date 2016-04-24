5e25be5a2da2ce9d6af6f2f0a15eb69b18c676b8
joshpaulchan <joshpaulchan@gmail.com>
2016-04-24 19:26:09 -0400

    Set up connection code on server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('client'));
 
-// app.get('/', function (req, res) {
-//   // res.send('Hello World!');
-// });
+io.on('connection', function(socket) {
+    console.log("A user connected");
+    socket.on('disconnect', function() {
+        console.log("A user disconnected");
+    });
+    
+    var gesture = Math.random();
+    io.emit('gesture', gesture);
+});
 
-app.listen(3000, function () {
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
