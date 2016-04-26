26a5dec889396c7a2f222c945385c4cac87cc7ad
kdmitriy93 <kdmitriy093@gmail.com>
2016-04-26 10:07:51 -0400

    Added files via upload

--- client/js/WindowObjController.js
+++ client/js/WindowObjController.js
@@ -17,6 +17,38 @@ var WindowObjController = function(scene) {
         this.windows[wdo.getId] = wdo;             // Track window
         // scene.add(wdo.obj);                 // Add window to scene
         
+        	Reticulum.add( wdo.obj, {
+	
+				clickCancelFuse: false, //load reticle when on an object
+				reticleHoverColor: 0x00fff6, //crosshair color 
+    			fuseVisible: true, //is visible
+    			fuseDuration: 0.5, //reticle takes 0.5 seconds to load
+    			fuseColor: 0xcc0000, //color
+    
+	onGazeOver: function()			//Function when reticle intersects the object
+	{
+	
+	},
+		
+	onGazeOut: function()			//Function when reticle moves off object
+	{
+				
+	},
+		
+	onGazeLong: function()		//Function for when the reticle has been on the window long enough for the fuse to load
+	{
+	
+	},
+		
+	onGazeClick: function()		//Function on Click
+	{
+
+	}		
+
+	});
+	scene.add( wdo.object );		//add objects to scene
+}
+        
         // TODO: register wdo with Reticulum
         // i.e. reticulum.add
         // on gaze enter: set WindowObjController.activewindow to this window
